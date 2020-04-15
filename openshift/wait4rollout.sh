277925616a14a9539af157c9016e76a769ecb7a4
Mattias.Poppe <poppe.mattias@gmail.com>
2020-04-15 22:36:18 +0200

    Fix roll-out script

--- openshift/wait4rollout.sh
+++ openshift/wait4rollout.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-### ARGS: $1 env
+### ARGS: $1 env $2 app-name
 ### script waits for next rollout
 set +x
 declare -i cur_ver
@@ -16,11 +16,10 @@ get_currentversion(){
       cur_ver=`oc rollout status deploymentconfig ${2}-${1}| awk '{print $3}'| sed 's/"//g'|rev|cut -d'-' -f1 |rev| tail -n1` 
    fi
    new_ver="$((cur_ver + 1))" || exit 1
-
 }
-get_currentversion $1
-oc rollout latest dc/${2}-${1}
 
+get_currentversion $1 $2
+oc rollout latest dc/${2}-${1}
 
 echo "${1} version to compare ${cur_ver}, new version ${new_ver}"
 
@@ -28,12 +27,9 @@ while true ; do
    if [ $new_ver -eq 1 ]  ;then
       echo "Maybe other rollout is going on sleeping.. "
       sleep 25
-      get_currentversion $1
+      get_currentversion $1 $2
       oc rollout latest dc/${2}-${1}
-
    fi
-
-
    oc rollout status deploymentconfig ${2}-${1} --revision=${new_ver}
    if [ $? -ne 0 ]; then
     sleep 40
